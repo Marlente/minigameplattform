9e57d130e7e1f994f02c34efd3b84f9327eb6a94
Marlente <marlo-k@gmx.de>
2022-07-27 12:44:42 +0800

    created scoreboard and count for RPS

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,18 +1,23 @@
-let rps = document.getElementById("play_rps");
-rps.addEventListener("click", function(){
-    playGame();
-    let playername= document.getElementById("name_rps").value;
-    let player_choice_rps = getUserChoice(document.getElementById("choice_rps").value);
-    if(player_choice_rps==="Invalid input"){
-      document.getElementById("choice_rps").innerHTML = player_choice_rps;
-    }
-    else{
-      let computer_choice_rps = getComputerChoice();
-      let result_rps = determineWinner(player_choice_rps, computer_choice_rps);
-      let display_result = document.createElement("li");
-      display_result.appendChild(result_rps);
-      document.getElementById("result_rps").appendChild(display_result);
-    }
+let play_rps = document.getElementById("play_rps");
+let count_rps_games = 0;
+let count_player_wins = 0;
+let count_computer_wins = 0;
+
+play_rps.addEventListener("click", function(){
+  let player_name_rps= document.getElementById("name_rps").value;
+  let player_choice_rps = getUserChoice(document.getElementById("choice_rps").value);
+  if(player_choice_rps==="Invalid input"){
+    document.getElementById("choice_rps").innerHTML = player_choice_rps;
+  }
+  else{
+    let computer_choice_rps = getComputerChoice();
+    let result_rps = determineWinner(player_choice_rps, computer_choice_rps);
+    let display_result = document.createElement("li");
+    display_result.appendChild(document.createTextNode(result_rps));
+    document.getElementById("result_rps").appendChild(display_result);
+
+    changeCountRPS(result_rps)
+  }
 });
 
 
@@ -42,9 +47,18 @@ const determineWinner = (userChoice, computerChoice) => {
   }
 }
 
+const changeCountRPS = (result) => {
+  if(result==="You win"){count_player_wins+=1;}
+  if(result==="You lose"){count_computer_wins+=1;}
+  count_rps_games+=1;
+
+  document.getElementById("games_rps").innerHTML = count_rps_games.toString();
+  document.getElementById("player_wins_rps").innerHTML = count_player_wins.toString();
+  document.getElementById("computer_wins_rps").innerHTML = count_computer_wins.toString();
+}
+
 function playGame(){
   let userChoice = getUserChoice("rock");
   let computerChoice = getComputerChoice();
   console.log(determineWinner(userChoice, computerChoice));
-
 }
